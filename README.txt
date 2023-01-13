39b59513763a46b417de89908151c8d7200f1210
Gonza <gcosta@alumno.unlam.edu.ar>
2023-01-13 01:10:51 -0300

    Resuelve el ejercicio planteado en el README

---
+++ README.txt
@@ -0,0 +1,3 @@
+Ejercicio Días Semana
+
+Esta práctica básica desarrollada en el lenguaje de programación JAVA propone ingresar un día de la semana y catalogarlo como "día hábil" o "fin de semana". A su vez, valida que el ingreso sea efectivamente un día de semana y no se trate de un input indeseado.
--- src/ar/com/gonzacostaweb/ejercicio_dias_semana/Ejercicio.java
+++ src/ar/com/gonzacostaweb/ejercicio_dias_semana/Ejercicio.java
@@ -1,9 +1,55 @@
 package ar.com.gonzacostaweb.ejercicio_dias_semana;
+import java.util.*;
 
 public class Ejercicio {
 	
 	public static void main(String[] args) {
 		
+		String[] diasSemana = {"lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"};
+		
+		Scanner entrada = new Scanner(System.in);
+				
+		System.out.println("Ingrese un día de la semana: ");
+		
+		System.out.println();
+		
+		String diaIngresado = entrada.next().toLowerCase();
+		
+		System.out.println();
+		
+		boolean checkDiaIngresado = false;
+		
+		for(int i = 0; i < diasSemana.length; i++) {
+			
+			if(diaIngresado.equals(diasSemana[i])) {
+				
+				checkDiaIngresado = true;
+				
+			}
+			
+		}
+		
+		if(checkDiaIngresado) {
+			
+			
+			if(diaIngresado.equals("sábado") || diaIngresado.equals("domingo")) {
+				
+				System.out.println("El " + diaIngresado + " es un día del fin de semana.");
+				
+			} else {
+				
+				System.out.println("El " + diaIngresado + " es un día hábil.");
+				
+			}
+		
+			
+		} else {
+			
+			System.out.println("La palabra ingresada está mal escrita o no es un día de semana.");
+			
+		}
+		
+		entrada.close();
 		
 		
 	}
